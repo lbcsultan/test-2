572ebd64d36c42c6898dc4e3cc0dad6e19f166ed
Byoungcheon <lbcsultan@gmail.com>
2024-09-26 17:08:19 +0900

    1st

---
+++ app/posts/new/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function NewPostPage() {
+  return (
+    <div>
+      <h1>New Post Page</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
+        molestiae incidunt sunt dicta quas deserunt, magnam ab quaerat ducimus
+        quae aspernatur at eum reprehenderit? Numquam distinctio impedit
+        accusamus quo totam.
+      </p>
+    </div>
+  )
+}
